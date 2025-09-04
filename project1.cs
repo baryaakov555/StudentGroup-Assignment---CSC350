ace2129cbc895f0484ebec30338008d3bb74b207
jakets123 <jake.torres123@gmail.com>
2025-09-04 14:40:19 -0400

    new1

--- project1.cs
+++ project1.cs
@@ -3,12 +3,37 @@ public class StudentGroup
     private string groupName;
     private int groupSize;
 
-    private string[] studentNames = new string[0];
-    private string studentName = "";
+    private string[] studentNames;
 
     public StudentGroup(string GroupName, int GroupSize)
     {
         groupName = GroupName;
         groupSize = GroupSize;
+        studentNames = new string[GroupSize];
+    }
+
+    public void AddStudent(string studentName, int position)
+    {
+        if (position < 0 || position > groupSize - 1)
+        {
+            Console.WriteLine("Position can not be a negative number");
+            return;
+        }
+
+        studentNames[position] = studentName;
+    }
+
+    public string GetStudent(int position)
+    {
+        return studentNames[position];
+    }
+
+    public void DisplayGroup()
+    {
+        Console.WriteLine($"Group: {groupName}");
+        foreach(string name in studentNames)
+        {
+            Console.WriteLine(studentNames);
+        }
     }
 }
